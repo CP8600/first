ed937e9ce4d76c23b739de74b5be94b9ec6074d9
Christopher <74881159+CP8600@users.noreply.github.com>
2021-01-04 00:44:21 -0600

    jan4update
    jsfun

--- github.js
+++ github.js
@@ -1,13 +1,28 @@
 let git = document.getElementById('git')
+let ol = document.getElementById('ol')
+let li = document.getElementById('li')
+
+const addText = 'World from '
+
+let firstName = document.getElementById('firstName')
+
+let y = firstName.innerHTML
+
+let x = document.getElementById('name')
+
+
+
 
 
 const gitThis = () => {
-   
-    let li = document.createElement('li')
-    
-    li.innerHTML = 'hello'
 
-  console.log(li)  
+  li.innerHTML = 'Hello ' + addText
+
+
 }
 
+
 gitThis()
+
+console.log(firstName)
+
