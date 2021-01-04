49af44033acf3cbfa604901a014123e37c53c7c4
Christopher <74881159+CP8600@users.noreply.github.com>
2021-01-04 01:13:02 -0600

    Add files via upload

---
+++ arrBox.js
@@ -0,0 +1,68 @@
+'use strict'
+
+/* let firstName
+let lastname
+let orderBuildersEfficiency = function () {
+
+const orderBuilders = [
+    { firstName: 'chris', lastname: 'perez', eff: 102 },
+    { firstName: 'mike', lastname: 'scott', eff: 99 },
+    { firstName: 'adam', lastname: 'sandler', eff: 86 }
+]
+
+
+first.innerHTML = orderBuilders
+}
+orderBuildersEfficiency()
+
+console.log(lastname)
+ */
+/* 
+var person = ["John", "Doe", 46];
+var person = { firstName: "John", lastName: "Doe", age: 46, firstName: 'chris' }
+
+console.log(person)
+ */
+
+/* let numList = function (value) {
+    return value <= 10
+
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+const list = [100, 99, 120, 118, 91, 122, 96]
+
+
+
+let x = document.getElementById('first')
+
+
+function effNotMet(list) {
+    
+ return list >= 100
+ 
+
+}
+
+
+effNotMet()
+
+
+function showEffNotMet(){
+
+x.innerHTML=effNotMet.
+
+
+
+}
+
