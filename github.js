320c5f594a35a77ffcda4dcc79d6cd3de01283e5
Christopher <74881159+CP8600@users.noreply.github.com>
2021-01-05 23:19:39 -0600

    main

--- github.js
+++ github.js
@@ -7,20 +7,30 @@ let names = document.getElementById('name')
 
 const addText = 'World from '
 
-let name = prompt('Whats your name?,  " " ')
+let name = prompt('Whats your name?  ')
 let bg = document.getElementById('body')
-
 const gitThis2 = () => {
- 
 
-
-bg.style.backgroundColor='red'
-bg.style.fontSize='2em'
-bg.style.color='white'
-bg.style.margin='10px'
+    bg.style.backgroundColor = 'red'
+    bg.style.fontSize = '2em'
+    bg.style.color = 'white'
+    bg.style.margin = '10px'
 }
 
+
 li.innerHTML = 'Hello ' + addText + name
+
+
+/* let i;
+for (i = 0; i < chooseColor.length; i++) {
+    text += cars[i] + "<br>";
+} */
+const setBg = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+}
+
 
 console.log(li.innerHTML)
 console.log(firstName.value)
